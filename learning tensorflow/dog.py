2c47bdd5f55ef3564d144c2034867e10b1b05e63
Ruchika Chavhan <32021556+bayesianGirl@users.noreply.github.com>
2018-07-02 05:24:20 +0000

    Dog Breed Classification

--- learning tensorflow/dog.py
+++ learning tensorflow/dog.py
@@ -15,35 +15,25 @@ data_dir= os.path.join(root, 'data')
 train_dir= os.path.join(data_dir, 'train')
 train_data=pd.read_csv(os.path.join(data_dir, 'labels.csv'))
 train_data.head()
-filenames=[]
-for images in os.listdir(train_dir):
-	filenames.append(images)
-	print(images)
-filename_queue = tf.train.string_input_producer(filenames)
-reader = tf.WholeFileReader()
-filename, content = reader.read(filename_queue)
-image = tf.image.decode_jpeg(content, channels=3)
-image = tf.cast(image, tf.float32)
-resized_image = tf.image.resize_images(image, [224, 224])
-image_batch = tf.train.batch([resized_image], batch_size=8)
 trainx=[]
 image_number=0
+label= train_data.label.values
 #reading images
 for images in os.listdir(train_dir):
 	image_path= os.path.join(train_dir, images)
 	img = imread(image_path, flatten=True)
 	img= cv2.resize(img, (100,100))
-	image_number=image_number+1
 	img = img.astype('float32')
 	tt= np.reshape(img, [10000])
-	trainx.append(tt)
+	if(label[image_number]!=121):
+		image_number=image_number+1
+		trainx.append(tt)
 	print(image_number)
 #dividing training data in train and cv sets
 split_size= int(len(trainx)*0.8)
 train_x, val_x = trainx[:split_size], trainx[split_size:]
 train_y, val_y = train_data.label.values[:split_size], train_data.label.values[split_size:]
 print(train_data.label.values)
-# print(train_x)
 #Defining the neural network
 input_units= 10000
 hidden_layer1= 1024
@@ -53,8 +43,8 @@ output_units= 120
 epochs= 5
 learning_rate=0.001
 batch_size=10
-x = tf.placeholder(tf.float32, [len(filenames), input_units])
-y = tf.placeholder(tf.float32, [len(filenames), output_units])
+x = tf.placeholder(tf.float32, [4, input_units])
+y = tf.placeholder(tf.float32, [4, output_units])
 #Initialising weights and biases
 print("DONE")
 w1= tf.get_variable('w1', initializer= tf.random_normal([input_units, hidden_layer1], seed= seed), dtype='float32')
@@ -71,39 +61,39 @@ hidden2= tf.nn.relu(tf.add(tf.matmul(hidden1, w2), b2))
 hidden3= tf.nn.relu(tf.add(tf.matmul(hidden2, w3), b3))
 output_layer= tf.sigmoid(tf.add(tf.matmul(hidden3, w4), b4))
 cost= tf.reduce_mean(tf.square(y-output_layer))
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
-optimizer.minimize(cost)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
 # Training the neural network
-y=train_y
-print(y)
-y_true= np.zeros([len(y), 120], dtype='float32')
+y_true= np.zeros([len(train_y), 120], dtype='float32')
 for f in range(0, len(y_true)):
-	h= y[f]
-	if(h!=121):
-		y_true[f][h-1]=1
-# enc= sklearn.preprocessing.OneHotEncoder()
-# y= pd.get_dummies(train_data.label.values)
-# enc.fit(y)
-# train_xi= tuple(train_x)
-# train_xz= tf.convert_to_tensor(train_x, dtype= tf.float32)
+	h= train_y[f]
+	y_true[f][h-1]=1
 sess= tf.InteractiveSession()
 sess.run(tf.global_variables_initializer())
-print(y_true)
-# train_z.eval(session=sess)
-# print(yi)
-# print(image_batch.eval(session=sess))
-for epoch in range(0, 12):
+for epoch in range(0, 3):
 	print("optimising")
-	_, c=sess.run(optimizer, feed_dict={x:train_x,y:y_true })		
-	print("Cost", c)
+	for i in range(0, len(train_x), 4):
+		temp_batch=[]
+		temp_batch_labels=[]
+		for j in range(i, i+4):
+			if(j<len(train_x)):
+				temp_batch.append(train_x[j])
+				temp_batch_labels.append(y_true[j])
+		if(len(temp_batch)==4):
+			_, c=sess.run([optimizer, cost], feed_dict={x:temp_batch,y:temp_batch_labels })
+			print(w1.eval(sess))		
+			print("Cost", c)
 #VAlidation error
+y_true_v= np.zeros([len(val_y), 120], dtype='float32')
+for f in range(0, len(y_true_v)):
+	h= val_y[f]
+	y_true_v[f][h-1]=1
 cost_v=0.0
 for i in range(0, len(val_x)):
-	hidden1= tf.nn.relu(tf.add(tf.matmul(val_x, w1), b1))
-	hidden2= tf.nn.relu(tf.add(tf.matmul(hidden1, w2), b2))
-	hidden3= tf.nn.relu(tf.add(tf.matmul(hidden2, w3), b3))
-	output_layer= tf.sigmoid(tf.add(tf.matmul(hidden3, w4), b4))
-	cost_v= cost_v+ tf.reduce_mean(tf.square(val_y-output_layer))
+	hidden1_v= tf.nn.relu(tf.add(tf.matmul(val_x, w1), b1))
+	hidden2_v= tf.nn.relu(tf.add(tf.matmul(hidden1_v, w2), b2))
+	hidden3_v= tf.nn.relu(tf.add(tf.matmul(hidden2_v, w3), b3))
+	output_layer_v= tf.sigmoid(tf.add(tf.matmul(hidden3_v, w4), b4))
+	cost_v= cost_v+ tf.reduce_mean(tf.square(y_true_v-output_layer_v))
 print("loss on validation set is ", cost_v)
 
 
